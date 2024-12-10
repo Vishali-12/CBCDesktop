19c86d90e1cc1fef72e11b8036e08e3b54244668
Ajay Shukla <141300540+AjayDev07@users.noreply.github.com>
2024-12-10 12:17:34 +0530

    RestartBugs

--- AutoStartApplication/Form1.cs
+++ AutoStartApplication/Form1.cs
@@ -2,7 +2,9 @@
 using AutoStartApplication.APIs;
 using Microsoft.Win32;
 using System;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Net.NetworkInformation;
 using System.Windows.Forms;
 
@@ -23,7 +25,8 @@ namespace AutoStartApplication
         }
 
         private void ConfigureNotifyIcon()
-        { // Create and configure a NotifyIcon
+        {
+            // Create and configure a NotifyIcon
             notifyIcon = new NotifyIcon()
             {
                 Icon = SystemIcons.Information,
@@ -47,9 +50,28 @@ namespace AutoStartApplication
             Application.Exit();
         }
 
+        //private void Form1_Load(object sender, EventArgs e)
+        //{
+        //    AddToStartup();
+        //    if (IsConnectedToInternet())
+        //    {
+        //        SyncData syncData = new SyncData();
+        //        DateTime yesterdayDate = DateTime.Today.AddDays(-1);
+        //        string fromDateTime = yesterdayDate.ToString("yyyy-MM-dd");
+        //        string toDateTime = DateTime.Now.ToString("yyyy-MM-dd");
+        //        var data = syncData.GetData(fromDateTime, toDateTime);
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Please Check Your Internet connetion");
+        //    }
+        //}
+
         private void Form1_Load(object sender, EventArgs e)
         {
+
             AddToStartup();
+            AddToStartupUsingTaskScheduler();
             if (IsConnectedToInternet())
             {
                 SyncData syncData = new SyncData();
@@ -60,8 +82,19 @@ namespace AutoStartApplication
             }
             else
             {
-                MessageBox.Show("Please Check Your Internet connetion");
+                MessageBox.Show("Please check your internet connection.");
             }
+
+            // Minimize to system tray
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+            NotifyIcon notifyIcon = new NotifyIcon
+            {
+                Icon = SystemIcons.Application,
+                Visible = true,
+                Text = "AutoStartApp Running"
+            };
+            //notifyIcon.ShowBalloonTip(1000, "AutoStartApp", "App is running in the background.", ToolTipIcon.Info);
         }
 
         private void AddToStartup()
@@ -71,16 +104,106 @@ namespace AutoStartApplication
                 string appName = "AutoStartApp";
                 string exePath = Application.ExecutablePath;
 
-                RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+                // Ensure the path is wrapped in quotes
+                string quotedExePath = $"\"{exePath}\"";
+                var bb = Registry.CurrentConfig;
 
-                registryKey.SetValue(appName, exePath);
+                using (RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true))
+                {
+                    if (registryKey == null)
+                    {
+                        throw new InvalidOperationException("Failed to open registry key.");
+                    }
 
+                    // Check if the entry already exist
+                    object existingValue = registryKey.GetValue(appName);
+                    if (existingValue != null && existingValue.ToString() == quotedExePath)
+                    {
+                        return; //Already set
+                    }
+
+                    registryKey.SetValue(appName, quotedExePath);
+                }
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Failed to set startup: " + ex.Message);
             }
         }
+
+        private void AddToStartupUsingTaskScheduler()
+        {
+            try
+            {
+                string exePath = Application.ExecutablePath;
+                string taskName = "AutoStartApp";
+                string taskCreationFlag = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "AutoStartAppTaskCreated.txt");
+
+                // Check if the task has already been created
+                if (File.Exists(taskCreationFlag))
+                {
+                    return;
+                }
+
+                // Create the task
+                ProcessStartInfo processInfo = new ProcessStartInfo
+                {
+                    FileName = "schtasks",
+                    Arguments = $"/Create /F /SC ONLOGON /TN \"{taskName}\" /TR \"\\\"{exePath}\\\"\"",
+                    Verb = "runas", // Requires admin privileges
+                    UseShellExecute = true
+                };
+
+                Process.Start(processInfo)?.WaitForExit();
+
+                // Mark the task as created
+                File.WriteAllText(taskCreationFlag, "Task created on " + DateTime.Now);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to set startup using Task Scheduler: " + ex.Message);
+            }
+        }
+
+
+
+        //private void AddToStartupUsingTaskScheduler()
+        //{
+        //    try
+        //    {
+        //        string exePath = Application.ExecutablePath;
+        //        string taskName = "AutoStartApp";
+
+        //        Process.Start(new ProcessStartInfo
+        //        {
+        //            FileName = "schtasks",
+        //            Arguments = $"/Create /F /SC ONLOGON /TN \"{taskName}\" /TR \"\\\"{exePath}\\\"\"",
+        //            Verb = "runas", // Ensure this runs with admin privileges
+        //            UseShellExecute = true
+        //        });
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("Failed to set startup using Task Scheduler: " + ex.Message);
+        //    }
+        //}
+
+
+        //private void AddToStartup()
+        //{
+        //    try
+        //    {
+        //        string appName = "AutoStartApp";
+        //        string exePath = Application.ExecutablePath;
+        //        RegistryKey registryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Run", true);
+        //        registryKey.SetValue(appName, $"\"{exePath}\"");
+
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show("Failed to set startup: " + ex.Message);
+        //    }
+        //}
 
         private void btnSync_Click(object sender, EventArgs e)
         {
@@ -98,7 +221,6 @@ namespace AutoStartApplication
             this.Hide();
             HistoryForm historyForm = new HistoryForm();
             historyForm.Show();
-
         }
 
         /// <summary>
